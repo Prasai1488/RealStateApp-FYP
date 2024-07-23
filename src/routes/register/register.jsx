ef4297bfe9f3a53dc58b73a829a0802112705acc
Prasai1488 <prasaiprithvi4@gmail.com>
2024-07-23 21:01:34 +0545

    confirmPassword

--- src/routes/register/register.jsx
+++ src/routes/register/register.jsx
@@ -1,6 +1,6 @@
 import "./resgister.scss";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+
 import { useState } from "react";
 import apiRequest from "../../lib/apiRequest";
 
